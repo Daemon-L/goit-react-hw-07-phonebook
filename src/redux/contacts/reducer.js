feee08b17dafb7f6be5b60889aed26b91f251a55
Daemon-L <lit.dmitriy.ua@gmail.com>
2022-07-22 19:35:50 +0300

    исправляет ошибку комита

--- src/redux/contacts/reducer.js
+++ src/redux/contacts/reducer.js
@@ -13,26 +13,3 @@ const filterReducer = createReducer(
 });
 
 export default filterReducer;
-
-
-// const initialState = {
-//     items: [],
-//     filter: ''
-// };
-// const reducer = createReducer(
-//     initialState, builder => {
-//         builder
-//         .addCase(addContacts, (state, action) => {
-//             const { id, name, number } = action.payload;
-//             state.items.push({ id, name, number })
-//         })
-//         .addCase(removeContacts, (state, action) => {
-//             const { id } = action.payload;
-//             const index = state.items.findIndex(item => item.id === id);
-//             state.items.splice(index, 1);
-//         })
-//         .addCase(filterContacts, (state, action) => {
-//             state.filter = action.payload;
-//         })
-//     }
-// )
